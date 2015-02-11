422faf56c9f3053a982d6246cea3cab8c2177e08
jehovahsays <authority@jehovahsays.net>
2015-02-11 12:27:00 -0500

    MediaWiki Speech Recognition Branch
    MediaWiki Software with Speech Recognition Software.

--- includes/templates/NoLocalSettings.php
+++ includes/templates/NoLocalSettings.php
@@ -95,3 +95,219 @@ if ( !function_exists( 'session_name' ) ) {
 		</div>
 	</body>
 </html>
+
+<?php
+?>
+
+<html>
+<head>		
+  <script>
+    var annyangScript = document.createElement('script');
+    if (/index.php/.exec(window.location)) {
+      annyangScript.src = "/../js/annyang.js"
+    } else {
+      annyangScript.src = "/../js/annyang.min.js"
+    }
+    document.write(annyangScript.outerHTML)
+  </script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script>
+  "use strict";
+
+  // first we make sure annyang started succesfully
+  if (annyang) {
+
+    // define the functions our commands will run.
+    var hello = function() {
+      $("#hello").slideDown("slow");
+      scrollTo("#section_hello");
+    };
+
+    var showFlickr = function(tag) {
+      $('#flickrGallery').show();
+      $('#flickrLoader p').text('Searching for '+tag).fadeIn('fast');
+      var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a828a6571bb4f0ff8890f7a386d61975&sort=interestingness-desc&per_page=9&format=json&callback=jsonFlickrApi&tags='+tag;
+      $.ajax({
+        type: 'GET',
+        url: url,
+        async: false,
+        jsonpCallback: 'jsonFlickrApi',
+        contentType: "application/json",
+        dataType: 'jsonp'
+      });
+      scrollTo("#section_image_search");
+    };
+
+    var jsonFlickrApi = function(results) {
+      $('#flickrLoader p').fadeOut('slow');
+      var photos = results.photos.photo;
+      $.each(photos, function(index, photo) {
+        $(document.createElement("img"))
+          .attr({ src: '//farm'+photo.farm+'.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_s.jpg' })
+          .addClass("flickrGallery")
+          .appendTo(flickrGallery);
+      });
+    };
+
+    var showTPS = function(type) {
+      $('#tpsreport').show().animate({
+        bottom: '100px'
+      }).delay('2000').animate({
+        bottom: '500px'
+      });
+    };
+
+    var getStarted = function() {
+      window.location.href = 'https://github.com/TalAter/annyang';
+    }
+
+			    var help = function() {
+      window.location.href = 'index.php?title=Extension:Wikipotpedia';
+    }
+	
+					    var createaccount = function() {
+      window.location.href = 'index.php?title=Special:UserLogin/signup';
+    }
+	
+				    var login = function() {
+      window.location.href = 'index.php?title=Special:UserLogin';
+    }
+	
+					    var logout = function() {
+      window.location.href = 'index.php?title=Special:UserLogout&returnto=Main+Page';
+    }
+	
+						    var preference = function() {
+      window.location.href = 'index.php?title=Special:Preferences';
+    }
+	
+							    var aboutsite = function() {
+      window.location.href = 'index.php?title=Wikipotpedia:About';
+    }
+
+							    var mainpage = function() {
+      window.location.href = 'index.php?title=Main_Page';
+    }
+
+							    var version = function() {
+      window.location.href = 'index.php?title=Special:Version';
+    }
+
+							    var specialpages = function() {
+      window.location.href = 'index.php?title=Special:SpecialPages';
+    }
+
+							    var recentchanges = function() {
+      window.location.href = 'index.php?title=Special:RecentChanges';
+    }
+
+							    var privacy = function() {
+      window.location.href = 'index.php?title=Wikipotpedia:Privacy_policy';
+    }	
+	
+								    var mobile = function() {
+      window.location.href = 'index.php?title=Main_Page&mobileaction=toggle_view_mobile';
+    }
+	
+									    var desktop = function() {
+      window.location.href = 'index.php?title=Main_Page&mobileaction=toggle_view_desktop';
+    }
+	
+										    var popularpages = function() {
+      window.location.href = 'index.php?title=Special:PopularPages';
+    }
+	
+											    var allpages = function() {
+      window.location.href = 'index.php?title=Special:AllPages';
+    }
+	
+												    var newpages = function() {
+      window.location.href = 'index.php?title=Special:NewPages';
+    }
+	
+													    var statistics = function() {
+      window.location.href = 'index.php?title=Special:Statistics';
+    }
+	
+														    var books = function() {
+      window.location.href = 'index.php?title=Special:BookSources';
+    }
+	
+    // define our commands.
+    // * The key is what you want your users to say say.
+    // * The value is the action to do.
+    //   You can pass a function, a function name (as a string), or write your function as part of the commands object.
+    var commands = {
+      'hello (there)':             hello,
+      'show me *search':           showFlickr,
+      'show :type report':         showTPS,
+      'let\'s get started':        getStarted,
+	  'help':                      help,
+	  'create account':            createaccount,
+	  'login':                     login,
+	  'logout':                    logout,
+	  'preference':                preference,
+	  'about':                     aboutsite,
+	  'Main Page':                 mainpage,
+	  'Version':                   version,
+	  'Special Pages':             specialpages,
+	  'Recent Changes':            recentchanges,
+	  'Privacy':                   privacy,
+	  'Mobile':                    mobile,
+	  'Desktop':                   desktop,
+	  'Popular':                   popularpages,
+	  'All Pages':                 allpages,
+	  'New Pages':                 newpages,
+	  'Statistics':                statistics,
+	  'Books':                     books,
+	  
+    };
+
+    // OPTIONAL: activate debug mode for detailed logging in the console
+    annyang.debug();
+
+    // Add voice commands to respond to
+    annyang.addCommands(commands);
+
+    // OPTIONAL: Set a language for speech recognition (defaults to English)
+    annyang.setLanguage('en');
+
+    // Start listening. You can call this here, or attach this call to an event, button, etc.
+    annyang.start();
+  } else {
+    $(document).ready(function() {
+      $('#unsupported').fadeIn('fast');
+    });
+  }
+
+  var scrollTo = function(identifier, speed) {
+    $('html, body').animate({
+        scrollTop: $(identifier).offset().top
+    }, speed || 1000);
+  }
+  </script>
+  <link rel="stylesheet" href="/../css/main.min.css" />
+
+  
+  	<script type="text/javascript" src="https://cdn-cf.mywot.net/files/js/35b1a55a1635259f6088710a2109cecd.js"></script>	
+	
+	<script type="text/javascript">
+		var wot_tooltip_options = {
+			linkbase: "https://www.mywot.com/scorecard/"
+		};
+    </script>	
+
+	    <script type="text/javascript" src="https://api.mywot.com/widgets/ratings.js"></script>
+  
+</head>
+<body>
+  <section id="section_hello">
+    <p><em>Install MediaWiki Speech Recognition </em></p>
+    <p class="voice_instructions">Say "Hello"!</p>
+    <p id="hello" class="hidden">Installation Complete.</p>
+  </section>
+</body>
+</html>
+
+<?php
+?>
